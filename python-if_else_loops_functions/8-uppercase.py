6557ea3e821eef8d0331908a2296f5f73d698a7f
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-04-29 23:04:15 +0000

    add my file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            # Kiçik hərfi böyük hərfə çevirmək üçün 32 çıxarırıq
+        # Kiçik hərfləri yoxlayırıq və onları böyük hərflərə çeviririk
+        if 'a' <= c <= 'z':
             print(chr(ord(c) - 32), end="")
         else:
-            # Böyük hərf və ya qeyri-hərf olanları olduğu kimi çap edirik
             print(c, end="")
-    print()  # Yeni xətt
+    print()  # Yeni xət
