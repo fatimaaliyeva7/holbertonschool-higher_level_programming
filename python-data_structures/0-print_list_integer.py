fe802bf5d8dd5b92bee4d33423b503547d914a9d
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-01 18:06:17 +0000

    my task2

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if isinstance(i, int):  # Ensure the item is an integer
-            print("{}".format(i))
-        else:
-            raise TypeError("All elements must be integers")
+        if not isinstance(i, int):
+            raise TypeError("list must contain only integers")
+        print("{:d}".format(i))
