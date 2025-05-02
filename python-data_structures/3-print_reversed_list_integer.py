96d3274b7a86377a01f956ac5f13d49cde6f61a2
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-02 09:18:10 +0000

    Fix: Use {:d} to print integers

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     """Prints all integers of a list in reverse order"""
     if my_list:  # Check if the list is not empty
         for i in reversed(my_list):  # Iterate through the list in reverse
-            print("{}".format(i))  # Print each integer
+            print("{:d}".format(i))  # Print each integer as a decimal number
