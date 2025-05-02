dc1270a55acd1fdc058958448d2727370147ba7f
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-02 09:59:57 +0000

    my task2

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position without modifying the original list"""
-    if idx < 0 or idx >= len(my_list):  
+    """Replaces an element in a list at a specific position
+    without modifying the original list"""
+    if idx < 0 or idx >= len(my_list):
         # Check if the index is out of range or negative
         return my_list.copy()  # Return a copy of the original list
     new_list = my_list.copy()  # Create a copy of the original list
