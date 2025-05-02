4d5d9c41ef5626e5c56fb07f0241193c75a7685a
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-02 09:08:07 +0000

    Fix: print_reversed_list_integer to handle all cases

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list:
+    """Prints all integers of a list in reverse order"""
+    if isinstance(my_list, list):
         for i in reversed(my_list):
-            print("{}".format(i))
+            if isinstance(i, int):
+                print("{}".format(i))
