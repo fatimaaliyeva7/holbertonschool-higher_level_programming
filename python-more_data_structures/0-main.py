5f76ed7732fe0126ed8854340ce861b0cf9ff99e
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-05 07:00:25 +0000

    my task

--- python-more_data_structures/0-main.py
+++ python-more_data_structures/0-main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+my_list = [1, 2, 3, 4, 5]
+# Test case 1: Print 2 elements from the list
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+# Test case 2: Print all elements from the list
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+# Test case 3: Print more elements than the list contains
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
