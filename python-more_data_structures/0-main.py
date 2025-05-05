c54f999a3d7f1887b58bb4823ca6843ece9e8211
fatimaaliyeva7 <eliyevaf777@gmail.com>
2025-05-05 07:06:22 +0000

    my task

--- python-more_data_structures/0-main.py
+++ python-more_data_structures/0-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
+safe_print_list = __import__('0-safe_print_list').safe_print_list  # funksiyanı import edirik
 my_list = [1, 2, 3, 4, 5]
-# Test case 1: Print 2 elements from the list
+# Test 1: Listdən 2 element çap edirik
 nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-# Test case 2: Print all elements from the list
+print("nb_print: {:d}".format(nb_print))  # Çap olunan elementlərin sayını göstəririk
+# Test 2: Listdəki bütün elementləri çap edirik
 nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
-# Test case 3: Print more elements than the list contains
+# Test 3: Listdə olan elementlərdən çoxunu çap etməyə çalışırıq
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
